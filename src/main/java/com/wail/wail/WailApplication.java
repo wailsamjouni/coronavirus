4b82c1af069d909ea095127fca02aa41ebbfce55
wailsamjouni <51680878+wailsamjouni@users.noreply.github.com>
2021-11-03 09:43:38 +0100

    Initial commit

---
+++ src/main/java/com/wail/wail/WailApplication.java
@@ -0,0 +1,13 @@
+package com.wail.wail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wail/wail/WailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wail.wail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
