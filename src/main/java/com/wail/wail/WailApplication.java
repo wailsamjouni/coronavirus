30e42c4dc6e4885191af0c7a05b53a8b4f529f0b
wailsamjouni <51680878+wailsamjouni@users.noreply.github.com>
2021-11-03 11:03:53 +0100

    get infos from URL

--- src/main/java/com/wail/wail/WailApplication.java
+++ src/main/java/com/wail/wail/WailApplication.java
@@ -2,8 +2,10 @@ package com.wail.wail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WailApplication {
 
     public static void main(String[] args) {
